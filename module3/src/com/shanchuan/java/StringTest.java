12a0caf66ca2bd32b66dfb6c358e8fb62ab8e823
shanchuan <15269978784@163.com>
2020-07-15 22:01:20 +0800

    Update to： Commonly used class 1.String class 3.Different String's instantiation methods

--- module3/src/com/shanchuan/java/StringTest.java
+++ module3/src/com/shanchuan/java/StringTest.java
@@ -9,6 +9,27 @@ import org.junit.Test;
 
 public class StringTest {
     /*
+    String的实例化方式：
+        1.字面量
+        2.new
+
+     */
+    @Test
+    public void test2(){
+        //1.声明在方法区中的字符串常量池，地址相同
+        String s1 = "javaEE";
+        String s2 = "javaEE";
+        //2.声明在堆中，地址不同
+        String s3 = new String("javaEE");
+        String s4 = new String("javaEE");
+
+        System.out.println(s1==s2);//true
+        System.out.println(s1==s3);//false
+        System.out.println(s3==s4);//false
+
+
+    }
+    /*
     String:字符串，使用一对“”引起来表示
         1.String声明为final，不可被继承
         2.String实现了Serializable接口：可序列化
@@ -20,8 +41,6 @@ public class StringTest {
             修改字符(relpace)
         5.通过字面量的方式给字符串变量赋值，此时值声明在字符串常量池
         6.字符串常量池中不会存储相同内容的字符串
-
-
      */
     @Test
     public void test1(){
