d5e6c968b8c52e62380465499b999bbf93190696
shanchuan <15269978784@163.com>
2020-07-18 19:15:36 +0800

    add:  Comparison between objects 1.comparable interface 2.comparator interface

--- module3/src/com/shanchuan/java2/CompareTest.java
+++ module3/src/com/shanchuan/java2/CompareTest.java
@@ -3,16 +3,20 @@ package com.shanchuan.java2;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * @Description: java对象之间的比较问题（comparable、comparator）
+ * comparable、comparator的比较：
+ *  comparable接口的方式一旦确定，保证实现comparable接口的类的对象在任何位置都可以比较大小
+ *  comparator接口的方式属于临时性
  * @Author: ShanChuan
  * @Data: 17:492020/7/18
  */
 public class CompareTest {
 
     /*
-        comparable的使用:
+        comparable的使用: 自然排序
             实现compareTo():
                 如果当前对象this大于形参对象obj，则返回正整数，如果当前对象this小于形参对象obj，则返回
                 负整数，如果当前对象this等于形参对象obj，则返回零。
@@ -37,6 +41,47 @@ public class CompareTest {
         System.out.println(Arrays.toString(arr));
 
     }
+
+    /*
+        comparator接口的使用：定制排序
+            1.当元素的类型没有实现java.lang.Comparable接口而又不方便修改代码，
+            或者实现了java.lang.Comparable接口的排序规则不适合当前的操作，
+            那么可以考虑使用 Comparator 的对象来排序
+            2.重写compare(Object o1,Object o2)方法，比较o1和o2的大小：如果方法返
+            回正整数，则表示o1大于o2；如果返回0，表示相等；返回负整数，表示
+            o1小于o2。
+     */
+    @Test
+    public void test3() {
+        String[] arr = new String[]{"AA", "CC", "KK", "MM", "GG", "JJ", "DD"};
+        //按照从大到小的顺序
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return -o1.compareTo(o2);
+            }
+        });
+        System.out.println(Arrays.toString(arr));
+    }
+
+    @Test
+    public void test4(){
+        Goods[] arr = new Goods[5];
+        arr[0] = new Goods("razer",134);
+        arr[1] = new Goods("xiaomi",99);
+        arr[2] = new Goods("罗技",399);
+        arr[3] = new Goods("罗技",22);
+        arr[4] = new Goods("华为",22);
+        Arrays.sort(arr, new Comparator<Goods>() {
+            @Override
+            public int compare(Goods o1, Goods o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+        System.out.println(Arrays.toString(arr));
+
+    }
+
 }
 
 //com.shanchuan.java2.Goods cannot be cast to java.lang.Comparable
