12139a4057b68eaf000a1e4ddfe12635914bca77
shanchuan <15269978784@163.com>
2020-07-23 16:30:21 +0800

    add: List接口概述

---
+++ module5/src/com/shanchuan/java1/ListTest.java
@@ -0,0 +1,19 @@
+package com.shanchuan.java1;
+
+/**
+ * @Description:
+ * 一、List接口概述
+ *       鉴于Java中数组用来存储数据的局限性，我们通常使用List替代数组
+ *       List集合类中元素有序、且可重复，集合中的每个元素都有其对应的顺序索引。
+ *       List容器中的元素都对应一个整数型的序号记载其在容器中的位置，可以根据
+ *      序号存取容器中的元素。
+ *       JDK API中List接口的实现类常用的有：
+ *          ArrayList：主要实现类，线程不安全、底层使用object[] elementDate
+ *          LinkedList：底层使用双向链表
+ *          Vector：古老实现类，线程安全、object[] elementDate
+ *
+ * @Author: ShanChuan
+ * @Data: 15:052020/7/23
+ */
+public class ListTest {
+}
