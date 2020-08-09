a05d58ef680082a1729e134d49eb16028a746c14
shanchuan <15269978784@163.com>
2020-08-09 15:56:00 +0800

    add:测试用Person类
    Signed-off-by: shanchuan <15269978784@163.com>

---
+++ module9/src/com/shanchuan/java/Person.java
@@ -0,0 +1,55 @@
+package com.shanchuan.java;
+
+/**
+ * @Description:
+ * @Author: ShanChuan
+ * @Data: 15:532020/8/8
+ */
+public class Person {
+
+    private String name;
+    public int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    private Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public void show(){
+        System.out.println("hello");
+    }
+    private void showNation(){
+        System.out.println("中国");
+    }
+}
